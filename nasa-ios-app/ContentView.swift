92fd12a4a66fa77669f0c3f0c4eb25236f667431
Sérgio Silva <sergiogsilva14@gmail.com>
2023-05-24 16:18:07 +0100

    new function

--- nasa-ios-app/ContentView.swift
+++ nasa-ios-app/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, joao!")
+            Text("hello2jkhasgwfjkrg, joao!")
         }
         .padding()
     }
