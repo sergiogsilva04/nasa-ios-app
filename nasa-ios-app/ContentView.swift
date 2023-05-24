1c1282859af5db261f5dd2a9c2db708f8c8676d7
Sérgio Silva <sergiogsilva14@gmail.com>
2023-05-24 15:46:06 +0100

    cocommit teste dfocioejoh

--- nasa-ios-app/ContentView.swift
+++ nasa-ios-app/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, teste!")
+            Text("Hello, yet2wgrdhjkwdfgvwjgh!")
         }
         .padding()
     }
